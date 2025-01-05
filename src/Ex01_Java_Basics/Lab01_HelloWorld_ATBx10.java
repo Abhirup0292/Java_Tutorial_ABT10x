9a3e33f1baa4b6b789e344f09cb317ea3a4b1740
abhirup <abhirupworkplace@gmail.com>
2025-01-05 01:40:24 -0500

    Commit 1

---
+++ src/Ex01_Java_Basics/Lab01_HelloWorld_ATBx10.java
@@ -0,0 +1,10 @@
+package Ex01_Java_Basics;
+
+public class Lab01_HelloWorld_ATBx10 {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/Ex01_Java_Basics/Lab2_add.java
@@ -0,0 +1,10 @@
+package Ex01_Java_Basics;
+
+public class Lab2_add {
+
+    public static void main(String[] args) {
+
+        System.out.println(2+2);
+    }
+
+}
